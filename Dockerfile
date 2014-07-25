5648c2da93273d34816c14411d9213fb2164391b
Ron Waldon <jokeyrhyme@gmail.com>
2014-07-26 09:42:15 +1000

    add NewRelic integration

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ RUN apt-get update -y
 RUN apt-get install php5-cli -y
 #RUN apt-get install nodejs npm -y
 #RUN apt-get install ruby2.0 -y
+
+#ENV NEWRELIC_LICENSE
+#ENV NEWRELIC_NAME
--- omnilint-server.go
+++ omnilint-server.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+  "os"
+  "github.com/go-martini/martini"
+  "github.com/yvasiyarov/gorelic"
+)
 
 func main() {
+  agent := gorelic.NewAgent()
+  agent.Verbose = true
+  agent.NewrelicLicense = os.Getenv("NEWRELIC_LICENSE")
+  agent.NewrelicName = os.Getenv("NEWRELIC_NAME")
+  agent.Run()
+
   m := martini.Classic()
   m.Get("/", func() string {
     return "Hello world!"
