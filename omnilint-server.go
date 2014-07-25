32d6e1ec6786412bb73927ccc1ef68570b843d6e
Ron Waldon <jokeyrhyme@gmail.com>
2014-07-26 09:51:17 +1000

    make NewRelic integration optional

--- omnilint-server.go
+++ omnilint-server.go
@@ -7,11 +7,17 @@ import (
 )
 
 func main() {
+  var (
+    newrelic_license = os.Getenv("NEWRELIC_LICENSE")
+    newrelic_name = os.Getenv("NEWRELIC_NAME")
+  )
+  if newrelic_license != "" && newrelic_name != "" {
   agent := gorelic.NewAgent()
   agent.Verbose = true
   agent.NewrelicLicense = os.Getenv("NEWRELIC_LICENSE")
   agent.NewrelicName = os.Getenv("NEWRELIC_NAME")
   agent.Run()
+  }
 
   m := martini.Classic()
   m.Get("/", func() string {
